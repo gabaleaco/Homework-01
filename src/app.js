788c1d34ceec5d8487371e92d86ede78aa167deb
Alex A <77032038+gabaleaco@users.noreply.github.com>
2022-04-08 07:36:09 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 // Place your DOM creation code here
+const header = document.createElement('h1');
+header.textContent = 'Homework 01';
+
+const p1 = document.createElement('p1');
+p1.textContent = 'paragraph 1';
+
+const p2 = document.createElement('p2');
+p2.textContent = 'paragraph 2';
+
+const mario = document.createElement('mario');
+mario.setAttribute('href', '\assets\mario.png');
+mario.textContent = 'Mario says "Wahoo!" to homework';
+mario.setAttribute('href', '\assets\Super Mario Bros. medley.mp3');
+
+const div = document.createElement('div');
+div.appendChild(header);
+div.appendChild(p1);
+div.appendChild(p2);
+div.appendChild(mario);
+
+document.body.appendChild(div);
