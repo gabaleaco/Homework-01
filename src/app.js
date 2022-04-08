2bb57df2f10fe3fbed992bc1cfa97e3a8237afad
Alex A <77032038+gabaleaco@users.noreply.github.com>
2022-04-08 09:19:02 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@
 const header = document.createElement('h1');
 header.textContent = 'Homework 01';
 
-const p1 = document.createElement('p1');
+const p1 = document.createElement('p');
 p1.textContent = 'For this homework, I want you to recreate this HTML page using JavaScript and the DOM API. This means, the HTML should have a blank body and minimal head. Once the page loads, use JavaScript to recreate all the elements from the original page. ';
 
-const p2 = document.createElement('p2');
+const p2 = document.createElement('p');
 p2.textContent = 'To make it fun, and maybe a bit tricky, I\'ve added several types of elements. Some elements are pretty simple like these \<p\> tags. Others are more going to require extra attributes or mixing text nodes with element nodes.';
 
 const mario = document.createElement('aside');
-mario.setAttribute('src', '/assets/mario.png');
+mario.setAttribute('src', 'assets/mario.png'); 
 mario.textContent = 'Mario says "Wahoo!" to homework';
-mario.setAttribute('src', '/assets/Super Mario Bros. medley.mp3');
+mario.setAttribute('src', 'assets/Super Mario Bros. medley.mp3');
 
 const div = document.createElement('div');
 div.appendChild(header);
