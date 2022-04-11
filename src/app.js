c48871d33944d7d8b05f3a45a90fb48dc865182c
Alex A <77032038+gabaleaco@users.noreply.github.com>
2022-04-11 09:18:33 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 // Place your DOM creation code here
+
+// const section = document.createElement('section');
+// section.appendChild(header);
+// section.appendChild(p1);
+// section.appendChild(p2);
+
 const header = document.createElement('h1');
 header.textContent = 'Homework 01';
 
@@ -6,18 +12,31 @@ const p1 = document.createElement('p');
 p1.textContent = 'For this homework, I want you to recreate this HTML page using JavaScript and the DOM API. This means, the HTML should have a blank body and minimal head. Once the page loads, use JavaScript to recreate all the elements from the original page. ';
 
 const p2 = document.createElement('p');
-p2.textContent = 'To make it fun, and maybe a bit tricky, I\'ve added several types of elements. Some elements are pretty simple like these \<p\> tags. Others are more going to require extra attributes or mixing text nodes with element nodes.';
+p2.textContent = 'To make it fun, and maybe a bit tricky, I\'ve added several types of elements. Some elements are pretty simple like these \<p\> tags. Others are more going to require extra attributes or mixing text nodes with element nodes.'; 
 
-const mario = document.createElement('aside'); //why not moving aside?
-mario.setAttribute('src', 'assets/mario.png'); 
-// mario.getElementById('img').src = 'assets/mario.png'; //no - make id?
-mario.textContent = 'Mario says "Wahoo!" to homework';
-mario.setAttribute('src', 'assets/Super Mario Bros. medley.mp3');
+
+// const aside = document.createElement('aside');
+// aside.appendChild(mimg);
+// aside.appendChild(mquote);
+// aside.appendChild(mtalk);
+
+const mimg = document.createElement('aside');
+mimg.setAttribute('src', 'assets/mario.png');
+
+const mquote = document.createElement('aside');
+mquote.textContent = 'Mario says "Wahoo!" to homework';
+
+const mtalk = document.createElement('aside');
+mtalk.setAttribute('src', 'assets/Super Mario Bros. medley.mp3');
+
 
 const div = document.createElement('div');
 div.appendChild(header);
 div.appendChild(p1);
 div.appendChild(p2);
-div.appendChild(mario);
+div.appendChild(mimg);
+div.appendChild(mquote);
+div.appendChild(mtalk);
+
 
 document.body.appendChild(div);
