d48692f9220624e076c5d52d271c03301f396db5
Alex A <77032038+gabaleaco@users.noreply.github.com>
2022-04-12 11:41:28 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 // Place your DOM creation code here
 
-// const section = document.createElement('section'); //this isn't right
-// section.appendChild(header);
-// section.appendChild(p1);
-// section.appendChild(p2);
-
+//These fall under Section > Main > Body
 const header = document.createElement('h1');
 header.textContent = 'Homework 01';
 
@@ -15,11 +11,7 @@ const p2 = document.createElement('p');
 p2.textContent = 'To make it fun, and maybe a bit tricky, I\'ve added several types of elements. Some elements are pretty simple like these \<p\> tags. Others are more going to require extra attributes or mixing text nodes with element nodes.'; 
 
 
-// const aside = document.createElement('aside'); //neither is this
-// aside.appendChild(mimg);
-// aside.appendChild(mquote);
-// aside.appendChild(mtalk);
-
+//These fall under Aside > Body
 const mimg = document.createElement('aside');
 mimg.setAttribute('src', 'assets/mario.png');
 
@@ -30,14 +22,25 @@ const mtalk = document.createElement('aside');
 mtalk.setAttribute('src', 'assets/Super Mario Bros. medley.mp3');
 
 
-//I'm confused about what to do with section and aside down here...
-const div = document.createElement('div');
-div.appendChild(header);
-div.appendChild(p1);
-div.appendChild(p2);
-div.appendChild(mimg);
-div.appendChild(mquote);
-div.appendChild(mtalk);
+//Append sections into Main then Body
+const section = document.createElement('section');
+section.appendChild(header);
+section.appendChild(p1);
+section.appendChild(p2);
+
+const main = document.createElement('main');
+main.appendChild(section);
 
 
-document.body.appendChild(div);
+//Append asides into Body
+const aside = document.createElement('aside');
+aside.appendChild(mimg);
+aside.appendChild(mquote);
+aside.appendChild(mtalk);
+
+
+const body = document.createElement('body');
+body.appendChild(main);
+body.appendChild(aside);
+
+document.body.appendChild(body);
